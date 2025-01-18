61a7a793d2fb35898dd2f6b25fe551427389039c
Rifat Khan <rifat6633@gmail.com>
2025-01-18 10:15:41 +0600

    feat: add Sanguard Font.

--- src/lib/fonts/index.ts
+++ src/lib/fonts/index.ts
@@ -1,4 +1,5 @@
 import { Geist, Geist_Mono } from "next/font/google";
+import localFont from "next/font/local";
 
 export const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -8,4 +9,10 @@ export const geistSans = Geist({
 export const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
+});
+
+export const sanguardRegular = localFont({
+  src: "./SanguardRegular.otf",
+  variable: "--font-sanguard-regular",
+  weight: "100 900",
 });
