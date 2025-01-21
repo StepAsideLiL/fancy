9cb4793f577fdad2b748c9489a82deb92bfcd7c7
Rifat Khan <rifat6633@gmail.com>
2025-01-21 12:45:13 +0600

    refactor: move techs in constent.

--- src/app/_parts/TechnologiesDescription.tsx
+++ src/app/_parts/TechnologiesDescription.tsx
@@ -9,40 +9,9 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-import icons from "@/lib/icons";
 import Link from "next/link";
 import { motion } from "motion/react";
-
-const techs = [
-  {
-    title: "Next.js",
-    description: "The React Framework.",
-    icon: icons.Nextjs,
-    version: "v15",
-    href: "https://nextjs.org/",
-  },
-  {
-    title: "Tailwind CSS",
-    description: "The Utility-First CSS Framework.",
-    icon: icons.Tailwind,
-    version: "v3",
-    href: "https://tailwindcss.com/",
-  },
-  {
-    title: "Shadcn UI",
-    description: "Beautifully designed components.",
-    icon: icons.Shadcn,
-    version: "v2",
-    href: "https://ui.shadcn.com/",
-  },
-  {
-    title: "Motion",
-    description: "A modern animation library for React.",
-    icon: icons.Motion,
-    version: "v11",
-    href: "https://motion.dev/",
-  },
-];
+import { techs } from "@/lib/constent";
 
 const AnimateCard = motion.create(Card);
 
---
+++ src/lib/constent.ts
@@ -0,0 +1,32 @@
+import icons from "./icons";
+
+export const techs = [
+  {
+    title: "Next.js",
+    description: "The React Framework.",
+    icon: icons.Nextjs,
+    version: "v15",
+    href: "https://nextjs.org/",
+  },
+  {
+    title: "Tailwind CSS",
+    description: "The Utility-First CSS Framework.",
+    icon: icons.Tailwind,
+    version: "v3",
+    href: "https://tailwindcss.com/",
+  },
+  {
+    title: "Shadcn UI",
+    description: "Beautifully designed components.",
+    icon: icons.Shadcn,
+    version: "v2",
+    href: "https://ui.shadcn.com/",
+  },
+  {
+    title: "Motion",
+    description: "A modern animation library for React.",
+    icon: icons.Motion,
+    version: "v11",
+    href: "https://motion.dev/",
+  },
+];
