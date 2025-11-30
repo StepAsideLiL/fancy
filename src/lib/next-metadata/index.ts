395d2f8b8195147e510ca795b4107118ae545591
Rifat Khan <54655388+StepAsideLiL@users.noreply.github.com>
2025-11-30 21:41:00 +0600

    refactor: move site config to site-config.ts lib file and update next-metadata.ts

--- src/lib/next-metadata/index.ts
+++ src/lib/next-metadata/index.ts
@@ -1,47 +1,38 @@
 import type { Metadata } from "next";
+import siteConfig from "@/lib/site-config";
 
 export default function nextMetadata(
   title?: string,
   description?: string,
 ): Metadata {
-  const site = {
-    title: "Fancy",
-    description: "How fancy the web can be!",
-    author: {
-      name: "StepAsideLiL",
-      url: "https://github.com/StepAsideLiL",
-    },
-  };
-  const baseUrl = "http://localhost:3000";
-
   const ogSearchParams = new URLSearchParams();
-  ogSearchParams.set("title", title ? title : site.title);
+  ogSearchParams.set("title", title ? title : siteConfig.title);
 
   return {
-    title: title ? `${title} - ${site.title}` : site.title,
-    description: description ? description : site.description,
-    authors: site.author,
-    metadataBase: new URL(baseUrl),
+    title: title ? `${title} - ${siteConfig.title}` : siteConfig.title,
+    description: description ? description : siteConfig.description,
+    authors: siteConfig.author,
+    metadataBase: new URL(siteConfig.url),
     icons: {
       icon: [
         { url: "/icons/favicon.ico" },
-        new URL("/icons/favicon.ico", baseUrl),
+        new URL("/icons/favicon.ico", siteConfig.url),
         { url: "/icons/favicon-16x16.png", sizes: "16x16" },
         { url: "/icons/favicon-32x32.png", sizes: "32x32" },
       ],
     },
-    manifest: "/site.webmanifest",
+    manifest: "/siteConfig.webmanifest",
     openGraph: {
-      title: site.title,
-      description: site.description,
+      title: siteConfig.title,
+      description: siteConfig.description,
       type: "website",
-      url: site.author.url,
+      url: siteConfig.author.url,
       images: [
         {
           url: `/api/og?${ogSearchParams.toString()}`,
           width: 1200,
           height: 630,
-          alt: site.title,
+          alt: siteConfig.title,
         },
       ],
     },
---
+++ src/lib/site-config/index.ts
@@ -0,0 +1,12 @@
+const siteConfig = {
+  title: "Fancy",
+  description: "How fancy the web can be!",
+  url: process.env.NODE_ENV === "development" ? "http://localhost:3000" : "",
+  author: {
+    name: "StepAsideLiL",
+    url: "https://github.com/StepAsideLiL",
+  },
+};
+
+export type TSiteConfig = typeof siteConfig;
+export default siteConfig;
